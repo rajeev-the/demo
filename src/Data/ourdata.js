aec27013f1b1f1c4537150ddbac23ef0ff8dfbb7
Rajeev <rajeevranjan9560807144@gmail.com>
2025-06-23 16:55:33 +0530

    first commit

--- src/Data/ourdata.js
+++ src/Data/ourdata.js
@@ -2,6 +2,18 @@ import  anunal17  from "../assets/media/17th.jpg"
 import tunnel from "../assets/media/WhatsApp Image 2025-06-20 at 17.39.35_c494f39f.jpg"
 import anunal3 from "../assets/media/WhatsApp Image 2025-06-20 at 17.39.35_994495b4.jpg"
 import bauma from "../assets/media/WhatsApp Image 2025-06-20 at 17.39.35_fd661409.jpg"
+import thrid2 from "../assets/media/thrid2.jpg"
+import thrid3 from "../assets/media/thrid3.jpg"
+import thrid4 from "../assets/media/thrid4.jpg"
+import thrid5 from "../assets/media/thrid5.jpg"
+import thrid6 from "../assets/media/thrid6.jpg"
+import thrid7 from "../assets/media/thrid7.jpg"
+import world1 from "../assets/media/world1.jpeg"
+import world2 from "../assets/media/world2.jpeg"
+import world3 from "../assets/media/world3.jpeg"
+import world4 from "../assets/media/world4.jpeg"
+
+
  
 export const event_page = [
   {
@@ -11,7 +23,13 @@ export const event_page = [
     venue: "Le Meridien, New Delhi",
     date: "2024-11-25 to 2024-11-26",
     src:[
-anunal3
+anunal3,
+thrid2,
+thrid3,
+thrid4,
+thrid5,
+thrid6,
+thrid7
     ]
   },
   {
@@ -24,10 +42,10 @@ anunal3
   {
     id: 3,
     title: "Tunnelling Association of India",
-    organizer: "World Tunnel Day",
+    topic: "World Tunnel Day",
     venue: "Scope Convention Centre, Lodhi Road, New Delhi",
     date: "2024-12-04",
-    src:[tunnel]
+    src:[tunnel , world1, world2, world3, world4]
   },
   {
     id: 4,
